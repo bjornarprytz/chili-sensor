6cc1e1e3203b1fb254feafd0b1f78bea03d516ae
bjornarprytz <bjornarprytz@users.noreply.github.com>
2021-06-16 14:41:42 +0200

    Added code for the pH, and the hum/temp sensor

--- SimpleWebServerWiFi.ino
+++ SimpleWebServerWiFi.ino
@@ -1,5 +1,7 @@
+
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <DHT.h>
 #include <ArduinoJson.h>
 
 #include "arduino_secrets.h"
@@ -8,12 +10,47 @@ char ssid[] = SECRET_SSID; // your network SSID (name)
 char pass[] = SECRET_PASS; // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;          // your network key index number (needed only for WEP)
 
+/// pH
+
+#define Vref 4.95
+
+/// TEMP / HUM
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11 // DHT 11
+//#define DHTTYPE DHT22 // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT10   // DHT 10
+//#define DHTTYPE DHT20   // DHT 20
+
+/*Notice: The DHT10 and DHT20 is different from other DHT* sensor ,it uses i2c interface rather than one wire*/
+/*So it doesn't require a pin.*/
+#define DHTPIN 2          // what pin we're connected to（DHT10 and DHT20 don't need define it）
+DHT dht(DHTPIN, DHTTYPE); //   DHT11 DHT21 DHT22
+//DHT dht(DHTTYPE);         //   DHT10 DHT20 don't need to define Pin
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+#define T_H_COOLDOWN 2000
+#define PH_COOLDOWN 5000
+
+float humidity;
+float temperature;
+int pH;
+
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
 void setup()
 {
     Serial.begin(115200); // initialize serial communication
+
+    Wire.begin();
+
+    dht.begin();
 
     pinMode(LED_BUILTIN, OUTPUT); // set the LED pin mode
 
@@ -49,6 +86,68 @@ void setup()
 
 void loop()
 {
+    read_ph();
+    read_temp_hum();
+    serve(server);
+}
+
+unsigned long prevPhReading;
+
+void read_ph()
+{
+    unsigned long currMillis = millis();
+
+    if (currMillis - prevPhReading < PH_COOLDOWN)
+    {
+        return;
+    }
+
+    prevPhReading = currMillis;
+
+    int sensorValue;
+
+    long sensorSum;
+
+    for (int m = 0; m < 50; m++)
+    {
+        sensorValue = analogRead(A0); //Connect the PH Sensor to A0 port
+
+        sensorSum += sensorValue;
+    }
+
+    sensorValue = sensorSum / 50;
+
+    pH = 7 - 1000 * (sensorValue - 372) * Vref / 59.16 / 1023;
+}
+
+unsigned long prevTempHumReading = 0;
+
+void read_temp_hum()
+{
+    unsigned long currMillis = millis();
+
+    if (currMillis - prevTempHumReading < T_H_COOLDOWN)
+    {
+        return;
+    }
+
+    prevTempHumReading = currMillis;
+
+    float temp_hum_val[2] = {0};
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+
+    if (!dht.readTempAndHumidity(temp_hum_val))
+    {
+        humidity = temp_hum_val[0];
+        temperature = temp_hum_val[1];
+    }
+}
+
+/// SERVER
+
+void serve(WiFiServer server)
+{
     WiFiClient client = server.available(); // listen for incoming clients
 
     if (client)
@@ -64,11 +163,15 @@ void loop()
 
             if (currentLine.startsWith("GET /ph"))
             {
-                response = "ph: 420";
+                response = String(String(pH) + "pH");
             }
             else if (currentLine.startsWith("GET /hum"))
             {
-                response = "hum: 69";
+                response = String(String(humidity) + "%");
+            }
+            else if (currentLine.startsWith("GET /temp"))
+            {
+                response = String(String(temperature) + "*C");
             }
             else if (currentLine.startsWith("GET /ec"))
             {
@@ -76,7 +179,7 @@ void loop()
             }
             else if (currentLine.length() == 0)
             {
-                printOKHeader(client, response);
+                respondOK(client, response);
                 response = "";
                 break;
             }
@@ -130,7 +233,7 @@ String readLine(WiFiClient client)
     }
 }
 
-void printOKHeader(WiFiClient client, String response)
+void respondOK(WiFiClient client, String response)
 {
     client.println("HTTP/1.1 200 OK");
     client.println("Content-type:text/html");
